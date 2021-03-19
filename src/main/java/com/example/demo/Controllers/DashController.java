f0f1b8c0cf2251d87fc381f1a5ed1ede03c42206
DESKTOP-VBMVJ7Q\Dominic <idig0245@scs.ubbcluj.ro>
2021-03-19 23:39:53 +0200

    added ai predictions for all coins

--- src/main/java/com/example/demo/Controllers/DashController.java
+++ src/main/java/com/example/demo/Controllers/DashController.java
@@ -4,12 +4,15 @@ import com.example.demo.DTOs.CoinDTO;
 import com.example.demo.Mappers.CoinMapper;
 import com.example.demo.Services.DashService;
 import io.swagger.annotations.Api;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import java.io.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Api
@@ -54,5 +57,49 @@ public class DashController {
     @GetMapping("getAnualMin")
     public CoinDTO getAnualMin() {
         return CoinMapper.mapCointoCoinDTO(dashService.getAnualMin());
+    }
+
+    private static final String CSV_SEPARATOR = ",";
+
+    private static void writeToCSV(List<CoinDTO> coinDTOS) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("CSV\\dashs.csv"), "UTF-8"));
+            bw.write("Date" + CSV_SEPARATOR + "Price");
+            bw.newLine();
+            for (CoinDTO coinDTO : coinDTOS) {
+                String oneLine = coinDTO.getDate().replaceAll("\\-", "") +
+                        CSV_SEPARATOR +
+                        coinDTO.getPrice();
+                bw.write(oneLine);
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+        } catch (IOException ignored) {
+        }
+    }
+
+    @GetMapping("prediction")
+    public ResponseEntity<?> makePrediction() {
+        writeToCSV(CoinMapper.mapCoinListtoCoinDTOList(dashService.getLastX(50)));
+        LocalDate localDate = LocalDate.now().plusDays(1);
+        List<CoinDTO> coinDTOS = new ArrayList<>();
+        try {
+            String argument = localDate.toString().replaceAll("\\-", "");
+
+            ProcessBuilder pb = new ProcessBuilder("python", "E:\\licenta\\prediction\\venv\\dashPrediction.py", "" + argument + "-" + argument);
+            Process p = pb.start();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            for (int i = 0; i < 5; i++) {
+                String price = in.readLine();
+                String date = localDate.plusDays(i).toString();
+                coinDTOS.add(new CoinDTO(date, Double.parseDouble(price)));
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return ResponseEntity.ok(coinDTOS);
     }
 }
